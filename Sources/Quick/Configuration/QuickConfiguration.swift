aded2f05815057bc3a4180af89bd7e34f4ba730d
Reading Smash <reading.smash0s@icloud.com>
2022-01-20 11:18:16 -0800

    Test fix

--- Sources/Quick/Configuration/QuickConfiguration.swift
+++ Sources/Quick/Configuration/QuickConfiguration.swift
@@ -34,7 +34,7 @@ extension QuickConfiguration {
 
         var configurationSubclasses: [QuickConfiguration.Type] = []
         for index in 0..<classesCount {
-            print("\(index): \(class_getSuperclass(classes[Int(index)]))")
+            print("\(index): \(String(describing: class_getSuperclass(classes[Int(index)])))")
             guard
                 let subclass = classes[Int(index)],
                 let superclass = class_getSuperclass(subclass),
